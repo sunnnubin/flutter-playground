38b948e8bead5705017dcbc904a4e3480bdd9bc7
sunnnubin <ubin2ek@naver.com>
2023-06-26 18:16:07 +0900

    0626 처음 깃으로 올려본다.

--- lib/screen/hom_screen.dart
+++ lib/screen/hom_screen.dart
@@ -5,6 +5,34 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Title'),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "test",
+                  style: TextStyle(fontSize: 50),
+                ),
+                const SizedBox(
+                  height: 200,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    debugPrint('btn');
+                  },
+                  child: const Text("data"),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
